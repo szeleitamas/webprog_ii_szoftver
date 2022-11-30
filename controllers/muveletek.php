bf6188479f5401ff723fcd951eea2b22a2e6466a
suvegesj <103772348+suvegesj@users.noreply.github.com>
2022-11-30 20:17:53 +0100

    Update muveletek.php

--- controllers/muveletek.php
+++ controllers/muveletek.php
@@ -3,7 +3,7 @@
 class Muveletek_Controller
 {
     public $baseName = 'muveletek';  //meghatározni, hogy melyik oldalon vagyunk
-    public function main(array $vars) // a router által továbbított paramétereket kapja
+    public function main(array $vars) // a router által továbbított paramétereket kapja meg
     {
         //betöltjük a nézetet
         $view = new View_Loader($this->baseName."_main");
